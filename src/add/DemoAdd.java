950f9353b7066b7a29a10f42fade59521aae3075
SolianaYemane <155404423+SolianaYemane@users.noreply.github.com>
2024-01-05 12:08:57 -0800

    Update DemoAdd.java

--- src/add/DemoAdd.java
+++ src/add/DemoAdd.java
@@ -4,5 +4,9 @@ public class DemoAdd {
 	public static void main(String[] args) {
 		System.out.println("Happy New Year.."); 
 	}
+	public static void sub(int a, int b){
+	System.out.println(a-b); 
+		
+	} 
 
 }
